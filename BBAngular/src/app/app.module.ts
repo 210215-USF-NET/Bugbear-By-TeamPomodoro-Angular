494876dd555abd7f7d1fec414bbb9943bcfeafba
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-23 15:16:04 -0500

    base angular setup

--- BBAngular/src/app/app.module.ts
+++ BBAngular/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
