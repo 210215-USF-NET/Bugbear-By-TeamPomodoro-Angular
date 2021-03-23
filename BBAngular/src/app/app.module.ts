b6b8dd9b21204546277b809cb76a5b222cd92cd2
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-23 15:21:22 -0500

    added character and campaign components

--- BBAngular/src/app/app.module.ts
+++ BBAngular/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { LoginComponent } from './login/login.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    CharactersComponent,
+    CampaignsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'characters', component: CharactersComponent },
+      { path: 'campaign', component: CampaignsComponent },
     ]),
   ],
   providers: [],
