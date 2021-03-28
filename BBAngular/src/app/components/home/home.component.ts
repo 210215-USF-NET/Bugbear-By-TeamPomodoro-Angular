40c4eb4cf975cea4bbd3c9f4693a699d4f282a08
Jacob Gaughan <jakegaughan5@gmail.com>
2021-03-28 00:34:40 -0500

    logs in adds user to db

--- BBAngular/src/app/components/home/home.component.ts
+++ BBAngular/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { user } from 'src/app/models/user';
@@ -9,46 +9,8 @@ import { BBRESTService } from 'src/app/services/bb-rest.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  isLoggedIn : any;
-  error : any;
-  user2Add : user;
+export class HomeComponent{
+  constructor(private router: Router, public auth: AuthService) {
 
-  constructor(private router: Router, public auth: AuthService, private BBService : BBRESTService) {
-    this.user2Add =
-    {
-      userID: 0,
-      email: ''
-    }
   }
-
-  ngOnInit(): void {
-  }
-
-  login(){
-    this.auth.loginWithRedirect();
-
-    this.auth.isAuthenticated$.subscribe(result => {
-      this.isLoggedIn = result;
-    })
-    if(this.isLoggedIn === undefined)
-    {
-      
-    }
-    else{
-      this.auth.user$.subscribe(user => {
-        this.BBService.GetUserByEmail(user.email).subscribe(
-          result => result,
-          error => {
-            this.error = error.status;
-            if (this.error == 404) {
-              this.user2Add.email = user.email;
-              this.BBService.AddUser(this.user2Add).subscribe();
-            }
-          }
-        )
-      })
-    }
-  }
-
 }
