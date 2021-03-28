85742b56ced5546bb69120b65953481481bc9dbc
ZMFrench <inbox@zachfrench.com>
2021-03-28 01:13:17 -0400

    should allow users to view their stories again

--- BBAngular/src/app/components/stories/stories.component.ts
+++ BBAngular/src/app/components/stories/stories.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { story } from 'src/app/models/story';
-import { BBRESTService } from 'src/app/services/bb-rest.service';
-import { AuthService } from '@auth0/auth0-angular';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { story } from 'src/app/models/story'
+import { BBRESTService } from 'src/app/services/bb-rest.service'
+import { AuthService } from '@auth0/auth0-angular'
 
 @Component({
   selector: 'app-stories',
@@ -10,8 +10,8 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./stories.component.css']
 })
 export class StoriesComponent implements OnInit {
-  stories: story[] = [];
-  storyID : number;
+  stories: story[] = []
+  storyID : number
 
   constructor(private BBService: BBRESTService, private router: Router, public auth: AuthService) {
     auth.user$.toPromise
@@ -26,17 +26,17 @@ export class StoriesComponent implements OnInit {
               result.forEach(story => {
                 if(story.userID === user.userID)
                 {
-                  this.stories.push(story);
+                  this.stories.push(story)
                 }
-              });
+              })
             }
-          );
+          )
         }
       )
     })
   }
 
   GetStory(storyTitle: string) {
-    this.router.navigate(['story-details'], { queryParams: { story: storyTitle } });
+    this.router.navigate(['story-details'], { queryParams: { story: storyTitle } })
   }
 }
