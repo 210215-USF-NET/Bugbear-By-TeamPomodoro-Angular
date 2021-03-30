9d31900cae3fa0de9951b9b528562f0850ecb08c
Ulices Esqueda <uesqueda756@gmail.com>
2021-03-30 12:12:24 -0700

    Commented out come of the campaign to work for now

--- BBAngular/src/app/components/nav-sidebar/nav-sidebar.component.ts
+++ BBAngular/src/app/components/nav-sidebar/nav-sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { campaign } from 'src/app/models/campaign';
 import { BBRESTService } from 'src/app/services/bb-rest.service';
@@ -10,11 +11,35 @@ import { BBRESTService } from 'src/app/services/bb-rest.service';
 })
 export class NavSidebarComponent implements OnInit {
   campaign: campaign;
-  constructor(private BBService: BBRESTService, public auth: AuthService) {
 
+  constructor(private BBService: BBRESTService, private router: Router, private route: ActivatedRoute, public auth: AuthService) {
+    this.campaign = {
+      campaignID: 0,
+      campaignName: "",
+      description: "",
+      gameMasterID: 0,
+      campaignUsers: [],
+      userID: 0
+      //campaignCharacters: [],
+      //campaignEncounters: [],
+      //campaignLocations:[],
+      //campaignMaps: [],
+      //campaignNPCs: [],
+      //campaignStories:[]
+    }
   }
 
   ngOnInit(): void {
+    this.route.queryParams
+      .subscribe(
+        params => {
+          this.BBService.GetCampaign(params.campaign).subscribe(
+            foundCampaign => {
+              this.campaign = foundCampaign
+            }
+          )
+        }
+      )
   }
 
 }
