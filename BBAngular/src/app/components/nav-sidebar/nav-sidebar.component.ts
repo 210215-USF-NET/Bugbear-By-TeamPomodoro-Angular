a2355cdae2acd38432cd6887a26def9b01dc4172
Ulices Esqueda <uesqueda756@gmail.com>
2021-03-30 10:26:40 -0700

    Final adding everything to commit

--- BBAngular/src/app/components/nav-sidebar/nav-sidebar.component.ts
+++ BBAngular/src/app/components/nav-sidebar/nav-sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { campaign } from 'src/app/models/campaign';
+import { BBRESTService } from 'src/app/services/bb-rest.service';
 
 @Component({
   selector: 'app-nav-sidebar',
@@ -6,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-sidebar.component.css']
 })
 export class NavSidebarComponent implements OnInit {
+  campaign: campaign;
+  constructor(private BBService: BBRESTService, public auth: AuthService) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
