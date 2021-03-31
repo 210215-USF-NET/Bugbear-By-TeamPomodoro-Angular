208a3dad7c742b8bf3dca4dabf5e21a767adcb2e
ZMFrench <inbox@zachfrench.com>
2021-03-30 20:51:04 -0400

    fix

--- BBAngular/src/app/app.module.ts
+++ BBAngular/src/app/app.module.ts
@@ -168,6 +168,7 @@ const appRoutes: Routes = [
     RulesDetailComponent,
     GetEncountersComponent,
     AddEncountersComponent,
+    EditEncountersComponent,
     EditStoryComponent,
     AddCampaignComponent,
     EditCampaignComponent,
--- BBAngular/src/app/components/encounters/add-encounters/add-encounters.component.ts
+++ BBAngular/src/app/components/encounters/add-encounters/add-encounters.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { encounter } from 'src/app/models/encounter';
+import { location } from 'src/app/models/location';
 import { BBRESTService } from 'src/app/services/bb-rest.service';
 
 @Component({
@@ -10,18 +11,19 @@ import { BBRESTService } from 'src/app/services/bb-rest.service';
 })
 export class AddEncountersComponent implements OnInit {
   encToAdd : encounter;
+  setting: location;
   constructor(private BBService: BBRESTService, private router: Router) { 
+    this.setting = {
+      locationID: 0,
+      locationName: '',
+      locationDescription: ''
+    }
     this.encToAdd = 
     {
       encounterID: 0,
       encounterTitle: '',
       encounterDescription: '',
-      location: 
-        {
-          locationID: 0,
-          locationName: '',
-          locationDescription: ''
-        }
+      location: this.setting
     }
   }
 
